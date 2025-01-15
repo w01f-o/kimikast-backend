07153ab3260511038210bda77f3d3eff7a0e60c3
w01f-o <arsenimarchenko@bk.ru>
2025-01-15 17:13:40 +0300

    fix: Fixed global path

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import * as path from 'node:path';
     ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: path.join(__dirname, '..', 'static'),
+      serveRoot: '/kimikast-api',
     }),
     AuthModule,
     UserModule,
--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,10 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('/kimikast-api');
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true,
     exposedHeaders: ['set-cookie'],
   });
